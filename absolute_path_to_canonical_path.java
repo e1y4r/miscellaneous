42862a4b1f0dd129a5ab1120eabe915db4508b72
Elyar Adil <Elyar.Adil@gmail.com>
2019-04-08 16:22:26 +0800

    Add files via upload

---
+++ absolute_path_to_canonical_path.java
@@ -0,0 +1,25 @@
+class PathConvert {
+    private static boolean skip(String s) {
+        return s.equals("") || s.equals(".");
+    } 
+    public static String simplifyPath(String path) {
+        String[] splitedPath = path.split("/");
+        splitedPath = Stream.of(splitedPath).filter(s -> !skip(s)).toArray(String[]::new);
+        String result = "";
+        int jump = 0;
+        for(int i = splitedPath.length - 1; i >= 0; i--) {
+            String s = splitedPath[i];
+            if(s.equals("..")) {
+                jump++;
+            } else {
+                if(jump > 0) {
+                    jump -= 1;
+                    continue;
+                }
+                result = "/" + s + result;
+            }
+        }
+        if(result.equals("")) return "/";
+        return result;
+    }
+}
