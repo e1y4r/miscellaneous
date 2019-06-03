d7f933aef72b5850fa66c8793d98dfc07d9d1183
Elyar Adil <Elyar.Adil@gmail.com>
2019-06-03 10:02:15 +0800

    Update row_to_json.sql

--- row_to_json.sql
+++ row_to_json.sql
@@ -5,15 +5,15 @@ CREATE PROCEDURE ROW_TO_JSON (
 	IN _id_column_name VARCHAR(32),
 	IN _id_value VARCHAR(32)
 ) BEGIN
-	SET @JSON_PRAM = (SELECT GROUP_CONCAT(CONCAT("'", column_name, "',", column_name)) FROM information_schema.columns WHERE table_name = "railink_lcl_cabin");
+	SET @JSON_PRAM = (SELECT GROUP_CONCAT(CONCAT("'", column_name, "',", column_name)) FROM information_schema.columns WHERE table_name = _table_name);
 	
-	SET @SELECT_JSON_STMT = CONCAT("SELECT JSON_OBJECT(", @JSON_PRAM, ") FROM ",_table_name, " WHERE ", _id_column_name, " = '", _id_value, "'");
+	SET @SELECT_JSON_STMT = CONCAT("SELECT JSON_OBJECT(", @JSON_PRAM, ") FROM ", _table_name, " WHERE ", _id_column_name, " = '", _id_value, "'");
 	PREPARE SELECT_JSON_STMT FROM @SELECT_JSON_STMT;
 	EXECUTE SELECT_JSON_STMT;
 	DEALLOCATE PREPARE SELECT_JSON_STMT;
 END;
 
-CALL ROW_TO_JSON("railink_lcl_cabin", "id", "RL19032000001");
+CALL ROW_TO_JSON("table", "id", "31415926535");
 
 
 
